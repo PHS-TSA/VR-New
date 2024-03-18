8a6099e406d72d6af3bd09e68f74520e083d4d67
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-03-17 22:17:32 -0500

    made walls randomly spawn from different directions

--- game/zones/outside/World.gd
+++ game/zones/outside/World.gd
@@ -5,7 +5,7 @@ const Wall = preload("res://flying_wall.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%Start.start(5)
+	%Start.start(1)
 	pass # Replace with function body.
 
 
@@ -28,9 +28,51 @@ func _on_start_timeout():
 
 func _on_spawn_1_timeout():
 	print("work ",num)
-	var spawned:NormalWall = Wall.instantiate()
-	spawned.global_position = %SpawnLocation1.global_position 
-	spawned.direction = 1
-	add_child(spawned)
-	
+	normal_wall_spawn()
 	pass # Replace with function body.
+
+#use case switch with ran num for spawn location in wall spawn function
+#goona need to change code below to reflect the directiosn the walls need to go
+
+func normal_wall_spawn():
+	var random = randi_range(1,8)
+	print(random)
+	var spawned:NormalWall = Wall.instantiate()
+	match (random): #I know this isn't super efficient eli but the judges are just watching a video. its fine
+		1:
+			spawned.x_z = false
+			spawned.direction = 1
+			spawned.global_position = %SpawnLocation1.global_position 
+		2:
+			spawned.x_z = false
+			spawned.direction = 1
+			spawned.global_position = %SpawnLocation2.global_position 
+		3:
+			spawned.x_z = true
+			spawned.direction = 1
+			spawned.global_position = %SpawnLocation3.global_position
+			spawned.rotation.y = PI/2
+		4:
+			spawned.x_z = true
+			spawned.direction = 1
+			spawned.global_position = %SpawnLocation4.global_position 
+			spawned.rotation.y = PI/2
+		5:
+			spawned.x_z = false
+			spawned.direction = -1
+			spawned.global_position = %SpawnLocation5.global_position 
+		6:
+			spawned.x_z = false
+			spawned.direction = -1
+			spawned.global_position = %SpawnLocation5.global_position 
+		7:
+			spawned.x_z = true
+			spawned.direction = -1
+			spawned.global_position = %SpawnLocation7.global_position
+			spawned.rotation.y = PI/2
+		8:
+			spawned.x_z = true
+			spawned.direction = -1
+			spawned.global_position = %SpawnLocation8.global_position 
+			spawned.rotation.y = PI/2
+	add_child(spawned)
