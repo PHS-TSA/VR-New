d8b01457575827a8d4159ad29d4d3f3df3161dd0
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-06-21 15:31:17 -0500

    Initial commit

---
+++ VR-New-main/flying_wall.gd
@@ -0,0 +1,29 @@
+class_name NormalWall
+extends StaticBody3D
+
+
+var x_z:bool # true is x, false is z
+var direction:int
+
+var travelled_distance:float = 0.0
+const SPEED:float = 2 #base is 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if(x_z):
+		global_position.x += direction * delta * SPEED
+	else:
+		global_position.z += direction * delta * SPEED
+	travelled_distance += abs(direction) * delta * SPEED
+	if (travelled_distance > 40):
+		queue_free()
+	
+	pass
+
+func funny():
+	print("Taking the easy way out lol")
